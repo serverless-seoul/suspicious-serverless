288efbc3760a628c5cb49afdedca898881715666
MooYeol Prescott Lee <mooyoul@gmail.com>
2017-11-01 03:47:29 +0900

    add content dispatch api

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -6,6 +6,7 @@ import {
 } from "vingle-corgi";
 
 import * as Joi from "joi";
+import { ContentDispatcher } from "../../services/content_dispatcher";
 import { RedirectionResolver } from "../../services/redirection_resolver";
 
 export const routes: Routes = [
@@ -25,4 +26,28 @@ export const routes: Routes = [
       });
     },
   ),
+  Route.GET(
+    "/content", {
+      desc: "Dispatch content of given url",
+      operationId: "dispatchContent",
+    }, {
+      url: Parameter.Query(Joi.string().required()),
+    }, async function(this: RoutingContext) {
+      const url = this.params.url as string;
+
+      // We can't re-use chrome instance now because currently serverless-chrome is not stable.
+      // launching new chrome process will cause additional processing time (approx. 600ms)
+      const dispatcher = new ContentDispatcher(!!process.env.DISABLE_SERVERLESS_CHROME);
+
+      await dispatcher.launch();
+
+      const content = await dispatcher.dispatch(url, 3000);
+
+      await dispatcher.shutdown();
+
+      return this.json({
+        data: content,
+      });
+    },
+  )
 ];

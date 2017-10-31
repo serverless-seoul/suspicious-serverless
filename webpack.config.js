f3ee8fc231eb7ebd2f04c8d9b7c3ecab29a2da8f
MooYeol Prescott Lee <mooyoul@gmail.com>
2017-11-01 03:28:59 +0900

    migrate to webpack@3

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,27 @@
 const path = require('path');
-const webpack = require('webpack');
 
 module.exports = {
   entry: './src/api',
   target: 'node',
-  module: {
-    loaders: [
-      { test: /\.ts$/, loader: 'ts-loader' },
-      { test: /\.json$/, loader: 'json-loader' },
-    ],
-  },
   resolve: {
-    root: __dirname,
-    extensions: ['', '.ts', '.js', '.json'],
-    alias: {
-      "@client": path.join(__dirname, "client/dist"),
-    },
+    extensions: [
+      '.js',
+      '.json',
+      '.ts',
+      '.tsx'
+    ]
   },
   output: {
     libraryTarget: 'commonjs',
-    path: '.webpack',
-    filename: 'api.js', // this should match the first part of function handler in serverless.yml
+    path: path.join(__dirname, '.webpack'),
+    filename: 'api.js'
   },
-  externals: ['aws-sdk', 'electron'],
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.DedupePlugin(),
-    // new webpack.optimize.UglifyJsPlugin({ minimize: true, sourceMap: false, warnings: false }),
-  ],
+  module: {
+    loaders: [
+      { test: /\.ts$/, loader: 'ts-loader' }
+    ]
+  },
+  externals: [
+    'aws-sdk'
+  ]
 };

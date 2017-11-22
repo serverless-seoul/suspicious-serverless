3bf0e17a8bb8a06c1bf09a41661e92b96d11128d
MooYeol Prescott Lee <mooyoul@gmail.com>
2017-11-22 22:50:41 +0900

    small performance improvements by ignoring unnecessary resource requests like image/video/fonts

--- src/services/content_dispatcher.ts
+++ src/services/content_dispatcher.ts
@@ -38,6 +38,23 @@ export class ContentDispatcher {
 
     this.log("created new page");
 
+    await page.setRequestInterceptionEnabled(true);
+
+    page.on("request", (interceptedRequest) => {
+      switch (interceptedRequest.resourceType) {
+        case "Image":
+        case "Media":
+        case "Font": {
+          interceptedRequest.abort();
+          break;
+        }
+        default: {
+          interceptedRequest.continue();
+          break;
+        }
+      }
+    });
+
     page.on("framenavigated", (frame: puppeteer.Frame) => {
       if (frame === mainFrame) {
         const navigatedUrl = frame.url();
